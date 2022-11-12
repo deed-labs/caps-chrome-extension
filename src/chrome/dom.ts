731202723f3ba24f695cfaba029230233a45c7fd
AlexeySadkovich <sadkovich01@mail.ru>
2022-11-12 12:06:52 +0300

    add button over addresses

--- src/chrome/dom.ts
+++ src/chrome/dom.ts
@@ -1,5 +1,48 @@
 const NEAR_PATTERN: RegExp = /^(([a-z\d]+[-_])*[a-z\d]+\.)(near|testnet)$/;
 
+const getOffset = (element: Element): { top: number; left: number } => {
+  const rect = element.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY,
+  };
+};
+
+const createOpenButton = (
+  height: number,
+  top: number,
+  left: number
+): Element => {
+  let iconNode = document.createElement("img");
+  iconNode.setAttribute("src", chrome.runtime.getURL("icon.png"));
+  iconNode.setAttribute("height", (height * 0.8).toString());
+
+  let textNode = document.createElement("span");
+  textNode.textContent = "Find";
+  textNode.style.color = "#ffffff";
+
+  let buttonNode = document.createElement("div");
+  buttonNode.style.background =
+    "linear-gradient(276.21deg, #9381FF 4.18%, rgba(147, 129, 255, 1) 97.48%)";
+  buttonNode.style.padding = "2px 5px";
+  buttonNode.style.borderRadius = (height / 4).toString() + "px";
+
+  // Set position
+  buttonNode.style.position = "absolute";
+  buttonNode.style.top = top.toString() + "px";
+  buttonNode.style.left = left.toString() + "px";
+
+  // Set children layout
+  buttonNode.style.display = "flex";
+  buttonNode.style.flexDirection = "row";
+  buttonNode.style.alignItems = "center";
+
+  buttonNode.appendChild(iconNode);
+  buttonNode.appendChild(textNode);
+
+  return buttonNode;
+};
+
 const searchAddresses = () => {
   let treeWalker = document.createTreeWalker(
     document.body,
@@ -11,17 +54,27 @@ const searchAddresses = () => {
     let node = treeWalker.currentNode;
 
     if (node.nodeValue && NEAR_PATTERN.test(node.nodeValue)) {
-      let iconNode = document.createElement("imag");
-      iconNode.setAttribute("src", "/soul.png");
+      node.parentNode?.addEventListener("mouseover", () => {
+        const nodeOffset = getOffset(node.parentElement!);
 
-      let spanNode = document.createElement("span");
-      spanNode.appendChild(iconNode);
+        let openButton = createOpenButton(
+          node.parentElement!.offsetHeight,
+          nodeOffset.top,
+          nodeOffset.left
+        );
 
-      let parentNode = document.createElement("div");
-      parentNode.appendChild(spanNode);
-      parentNode.appendChild(node.cloneNode(true));
+        openButton.addEventListener("click", () => {
+          chrome.runtime.sendMessage("OpenPopup");
+        });
 
-      node.parentNode?.replaceChild(parentNode, node);
+        node.parentNode?.addEventListener("mouseout", () => {
+          try {
+            document.body.removeChild(openButton);
+          } catch {}
+        });
+
+        document.body.appendChild(openButton);
+      });
     }
   }
 };
